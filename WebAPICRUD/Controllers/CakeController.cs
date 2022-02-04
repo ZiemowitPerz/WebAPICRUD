4dc4142eb921038ce972d00b0d0eeaea3a593415
Ziemowit <Ziemowit@DESKTOP-LGP11BU>
2022-02-05 00:54:16 +0100

    Add project files.

---
+++ WebAPICRUD/Controllers/CakeController.cs
@@ -0,0 +1,25 @@
+﻿using WebAPICRUD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dot6.API.Crud.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CakeController : ControllerBase
+{
+    private readonly MyWorldDbContext _myWorldDbContext;
+    public CakeController(MyWorldDbContext myWorldDbContext)
+    {
+        _myWorldDbContext = myWorldDbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        //https://youtu.be/A6dPEYbGH0k?t=842
+        var cakes = await _myWorldDbContext.Cake.ToListAsync();
+        return Ok(cakes);
+    }
+
+}
---
+++ WebAPICRUD/Data/MyWorldDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPICRUD.Data.Entities;
+
+namespace WebAPICRUD.Data;
+
+public class MyWorldDbContext : DbContext
+{
+    public MyWorldDbContext(DbContextOptions<MyWorldDbContext> options): base(options)
+    {
+
+    }
+    public DbSet<Cake> Cakes { get; set; }
+    public object Cake { get; set; }
+}
+ 
