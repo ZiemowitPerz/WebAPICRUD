f8e26852e3a7998b69b9b5c319f48395b410ef30
Ziemowit Perz <zimekow@gmail.com>
2022-02-08 00:26:38 +0100

    added: HttpPost HttpPut HttpDelete

--- WebAPICRUD/Controllers/CakeController.cs
+++ WebAPICRUD/Controllers/CakeController.cs
@@ -1,6 +1,7 @@
 ﻿using WebAPICRUD.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebAPICRUD.Data.Entities;
 
 namespace Dot6.API.Crud.Controllers;
 
@@ -19,6 +20,44 @@ public class CakeController : ControllerBase
     {
         var cakes = await _myWorldDbContext.Cake.ToListAsync();
         return Ok(cakes);
+    }
+
+    [HttpGet]
+    [Route("get-cake-by-id")]
+    public async Task<IActionResult> GetCakeByIdAsync(int id)
+    {
+        var cake = await _myWorldDbContext.Cake.FindAsync(id);
+        return Ok(cake);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Cake cake)
+    {
+        _myWorldDbContext.Cake.Add(cake);
+        await _myWorldDbContext.SaveChangesAsync();
+        return Created($"/get-cake-by-id?id={cake.Id}", cake);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> PutAsync(Cake cakeToUpdate)
+    {
+        _myWorldDbContext.Cake.Update(cakeToUpdate);
+        await _myWorldDbContext.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [Route("{id}")]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var cakeToDelete = await _myWorldDbContext.Cake.FindAsync(id);
+        if (cakeToDelete == null)
+        {
+            return NotFound();
+        }
+        _myWorldDbContext.Cake.Remove(cakeToDelete);
+        await _myWorldDbContext.SaveChangesAsync();
+        return NoContent();
     }
 
 }
